675bdd2b27843077a96880dbcbff55d277f411d4
Jordan Parker <nepthar@gmail.com>
2023-10-20 08:13:11 -0600

    Use the rails provided Dockerfile, add workspace.sh

---
+++ workspace.sh
@@ -0,0 +1,6 @@
+# Shell workspace file - scripts and variables for this workspace or project
+# Use with github.com/nepthar/wksp for "magic" or just `source`
+workspace="herd" # This must be set
+
+# Other variables or functions
+# herd.run-tests() ...
