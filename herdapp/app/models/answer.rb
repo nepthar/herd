55f3500dbc1fbe9b877497ac2f8e5169247973ae
Jordan Parker <nepthar@gmail.com>
2023-10-30 13:52:26 -0600

    Validations, associations, attributes on Q and A models

--- herdapp/app/models/answer.rb
+++ herdapp/app/models/answer.rb
@@ -1,2 +1,9 @@
 class Answer < ApplicationRecord
+
+  attribute :value, :integer
+  attribute :created_by, :string
+
+  belongs_to :question
+
+  validates :value, inclusion: { in: 0..100 }
 end
--- herdapp/app/models/question.rb
+++ herdapp/app/models/question.rb
@@ -1,2 +1,17 @@
 class Question < ApplicationRecord
+
+  attribute :title, :string
+  attribute :question, :string
+  attribute :option1, :string
+  attribute :option2, :string
+  attribute :created_by, :string
+
+  has_many :answers
+
+  validates :title, length: { in: 1..255 }
+  validates :question, length: { in: 1..255 }
+  validates :option1, length: { in: 1..255 }
+  validates :option2, length: { in: 1..255 }
+  validates :created_by, length: { in: 1..255 }
+
 end
